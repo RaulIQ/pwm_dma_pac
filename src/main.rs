a697658caa7a1a9e316534a8b74e58987cc216e2
RaulIQ <raulalimbekov@gmail.com>
2024-11-08 16:40:10 +0300

    add functions which create a dshot frame

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,23 @@
 
 use cortex_m_rt::entry;
 use defmt::println;
-use stm32f7xx_hal::pac::{self, tim2::dmar};
+use stm32f7xx_hal::pac::{self};
 use {defmt_rtt as _, panic_probe as _};
 
+const ONE_THIRD: u16 = 120;
+const TWO_THIRD: u16 = 240;
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let rcc = dp.RCC;
-    let tim = dp.TIM3;
-    let gpio_a = dp.GPIOA;
-    let gpio_c = dp.GPIOC;
-    let dma1 = dp.DMA1;
-    let pwr = dp.PWR;
+const DSHOT_FRAME_SIZE: usize = 16; // 16 bits per frame
+static mut DSHOT_FRAME: [u16; DSHOT_FRAME_SIZE] = [0; DSHOT_FRAME_SIZE];
 
-    rcc.apb1enr.modify(|_, w| w.pwren().set_bit());
-    pwr.cr1.modify(|_, w| w.vos().scale1());
-    pwr.cr1.modify(|_, w| w.oden().set_bit());
-    while pwr.csr1.read().odrdy().bit_is_clear() {}
-    pwr.cr1.modify(|_, w| w.odswen().set_bit());
-    while pwr.csr1.read().odswrdy().bit_is_clear() {}
+fn init_outputs(dp: &pac::Peripherals) {
+    let rcc = &dp.RCC;
 
-    rcc.cr.modify(|_, w| w.hseon().on());
-    while rcc.cr.read().hserdy().is_not_ready() {}
-    println!("Hse ready");
-
-    rcc.pllcfgr.modify(|_, w| w.pllsrc().hse());
-    rcc.pllcfgr.modify(unsafe {|_, w| w.pllm().bits(4)});
-    rcc.pllcfgr.modify(unsafe {|_, w| w.plln().bits(108)});
-    rcc.pllcfgr.modify(|_, w| w.pllp().div2());
-
-    rcc.cr.modify(|_, w| w.pllon().on());
-    while rcc.cr.read().pllrdy().is_not_ready() {}
-    println!("Pll ready");
-
-    dp.FLASH.acr.modify(|_, w| w.latency().ws9());
-    while !dp.FLASH.acr.read().latency().is_ws9() {}
-
-    rcc.cfgr.modify(|_, w| w
-        .ppre1().div2()
-        .ppre2().div2()
-    );
-
-    rcc.cfgr.modify(|_, w| w.sw().pll());
-    while !rcc.cfgr.read().sws().is_pll() {}
-    println!("pll selectected");
-
-    rcc.apb1enr.write(|w| w.tim3en().set_bit());
-    rcc.ahb1enr.write(|w| w.gpioaen().set_bit());
+    rcc.ahb1enr.modify(|_, w| w.gpioaen().set_bit());
     rcc.ahb1enr.modify(|_, w| w.gpiocen().set_bit());
-    rcc.ahb1enr.modify(|_, w| w.dma1en().set_bit());
+
+    let gpio_a = &dp.GPIOA;
+    let gpio_c = &dp.GPIOC;
 
     gpio_a.moder.write(|w| w.moder6().alternate());
     gpio_a.otyper.write(|w| w.ot6().push_pull());
@@ -76,10 +43,17 @@ fn main() -> ! {
     gpio_c.otyper.modify(|_, w| w.ot9().push_pull());
     gpio_c.ospeedr.modify(|_, w| w.ospeedr9().very_high_speed());
     gpio_c.pupdr.modify(|_, w| w.pupdr9().floating());
-    gpio_c.afrh.modify(|_, w| w.afrh9().af2()); 
+    gpio_c.afrh.modify(|_, w| w.afrh9().af2());
+}
+
+fn init_tim3(dp: &pac::Peripherals) {
+    let rcc = &dp.RCC;
+    rcc.apb1enr.modify(|_, w| w.tim3en().set_bit());
+
+    let tim = &dp.TIM3;
 
     unsafe {
-        tim.arr.write(|w| w.bits(27)); // frequency
+        tim.arr.write(|w| w.bits(359)); // 600KHz
         tim.ccr1().write(|w| w.bits(0));// duty cycle
         tim.ccr2().write(|w| w.bits(0));
         tim.ccr3().write(|w| w.bits(0));
@@ -130,14 +104,23 @@ fn main() -> ! {
     // start pwm
     tim.cr1.modify(|_, w| w.cen().set_bit());
 
+}
+
+fn init_dma1(dp: &pac::Peripherals, dmar_addr: u32) {
+    let rcc = &dp.RCC;
+
+    rcc.ahb1enr.modify(|_, w| w.dma1en().set_bit());
+
+    let dma1 = &dp.DMA1;
+
     unsafe {
-        let dmar_addr = tim.dmar.as_ptr() as u32;
-        let dma_buf: [u16; 16] = [9, 9, 9, 9, 18, 18, 18, 18, 0, 0, 0, 0, 0, 0 ,0, 0];
+        dma1.st[2].fcr.modify(|_, w| w
+            .dmdis().set_bit()
+            .fth().full()
+        );
 
-        dma1.st[2].fcr.modify(|_, w| w.dmdis().set_bit());
-
-        dma1.st[2].m0ar.write(|w| w.m0a().bits(dma_buf.as_ptr() as u32));
-        dma1.st[2].ndtr.write(|w| w.ndt().bits(dma_buf.len() as u16));
+        dma1.st[2].m0ar.write(|w| w.m0a().bits(DSHOT_FRAME.as_ptr() as u32));
+        dma1.st[2].ndtr.write(|w| w.ndt().bits(DSHOT_FRAME_SIZE as u16));
         dma1.st[2].par.write(|w| w.pa().bits(dmar_addr));
 
         dma1.st[2].cr.reset();
@@ -145,18 +128,99 @@ fn main() -> ! {
             .chsel().bits(5)
             .mburst().incr4()
             .pburst().incr4()
-            .pl().high()
-            .msize().bits16() // value depends on the type of buffer elements 
-            .psize().bits16() // value depends on the type of buffer elements 
+            .pl().very_high()
+            .msize().bits16()
+            .psize().bits16() 
             .minc().set_bit()
             .pinc().clear_bit()
             .circ().enabled()
             .dir().memory_to_peripheral()
-            .teie().set_bit()
-            .htie().set_bit()
-            .tcie().set_bit()
+            .teie().clear_bit()
+            .htie().clear_bit()
+            .tcie().clear_bit()
             .en().enabled()
         );
+    }
+
+}
+
+fn init_power_and_clock(dp: &pac::Peripherals) {
+    let rcc = &dp.RCC;
+    let pwr = &dp.PWR;
+
+    rcc.apb1enr.modify(|_, w| w.pwren().set_bit());
+    pwr.cr1.modify(|_, w| w.vos().scale1().oden().set_bit());
+    while pwr.csr1.read().odrdy().bit_is_clear() {}
+    pwr.cr1.modify(|_, w| w.odswen().set_bit());
+    while pwr.csr1.read().odswrdy().bit_is_clear() {}
+
+    // Configure HSE as PLL source
+    rcc.cr.modify(|_, w| w.hseon().on());
+    while rcc.cr.read().hserdy().is_not_ready() {}
+    println!("HSE ready");
+
+    rcc.pllcfgr.modify(|_, w| w.pllsrc().hse());
+    rcc.pllcfgr.modify(unsafe { |_, w| w.pllm().bits(4) });
+    rcc.pllcfgr.modify(unsafe { |_, w| w.plln().bits(216) });
+    rcc.pllcfgr.modify(|_, w| w.pllp().div2());
+    rcc.cr.modify(|_, w| w.pllon().on());
+
+    while rcc.cr.read().pllrdy().is_not_ready() {}
+    println!("PLL ready");
+
+    dp.FLASH.acr.modify(|_, w| w.latency().ws7());
+    while !dp.FLASH.acr.read().latency().is_ws7() {}
+
+    rcc.dckcfgr1.modify(|_, w| w.timpre().mul4());
+    rcc.cfgr.modify(|_, w| w.ppre1().div4().ppre2().div2());
+    rcc.cfgr.modify(|_, w| w.sw().pll());
+    while !rcc.cfgr.read().sws().is_pll() {}
+    println!("PLL selected");
+}
+
+// Create DShot data packet
+fn create_dshot_packet(throttle: u16, telemetry: bool) -> u16 {
+    let mut packet = (throttle & 0x07FF) << 1; // 11-bit throttle
+    if telemetry {
+        packet |= 1; // Set telemetry bit if needed
+    }
+
+    let crc = ((packet ^ (packet >> 4) ^ (packet >> 8)) & 0x0F) as u16; // 4-bit CRC
+    (packet << 4) | crc
+}
+
+// Prepare DShot frame as array of pulse widths for each bit
+fn prepare_dshot_frame(packet: u16) {
+    for i in 0..DSHOT_FRAME_SIZE {
+        // Calculate each bit’s pulse width
+        let is_one = (packet & (1 << (15 - i))) != 0;
+        unsafe {
+            DSHOT_FRAME[i] = if is_one {
+                TWO_THIRD // Set for ~62.5% high pulse
+            } else {
+                ONE_THIRD // Set for ~37.5% high pulse
+            };
+        }
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let dp = &pac::Peripherals::take().unwrap();
+
+    let tim = &dp.TIM3;
+    init_power_and_clock(dp);
+    init_outputs(dp);
+    init_tim3(dp);
+
+    let dmar_addr = tim.dmar.as_ptr() as u32;
+    init_dma1(dp, dmar_addr);
+
+    let packet = create_dshot_packet(1046, false);
+    prepare_dshot_frame(packet);
+
+    unsafe {
+        println!("{}", DSHOT_FRAME);
     }
 
     loop {}
